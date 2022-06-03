04556f8df312895e18e28a323eba63a24fbe8eda
Broderick Crosby <bcrosby@cedar1.cedar.computecanada.ca>
2022-06-03 06:23:55 -0700

    first annotations added to code

--- snp_trim.sh
+++ snp_trim.sh
@@ -6,20 +6,24 @@
 #SBATCH --mem=8G
 #SBATCH --time=0-2:0:0
 #SBATCH -o snp_trim_%A.log
-#SBATCH -e snp_trim_%A.err
 
 
 #
 # This script requires the name of the raw fastq file directory as an argument,
-# e.g, "sbatch snp_trim.sh MiSeq_12_microsatellite"
+# e.g, "sbatch snp_trim.sh BRMS010"
 #
 
 
-module load fastqc
+# Load the necessary functions into the environment
+
+# Java is necessary for Trimmomatic
 module load java
+
+# Trimmomatic to perform quality-trimming of sequence reads
 module load trimmomatic
 
 
+# Create directory to contain trimmed sequences
 rm -fr $1_snp_trim/
 mkdir $1_snp_trim/
 mkdir $1_snp_trim/unpaired/
@@ -29,40 +33,40 @@ echo "########################################################"
 echo "# Trimming data for run: $1 "
 echo "########################################################"
 
-echo "########################################################" >> snp_trim_$SLURM_JOB_ID.err
-echo "# Trimming data for run: $1 " >> snp_trim_$SLURM_JOB_ID.err
-echo "########################################################" >> snp_trim_$SLURM_JOB_ID.err
 
-
+# List the files in the specified directory, then convert those filenames to sample names
 ls /home/bcrosby/projects/def-pawilson/caribou_MiSeq_project/$1/fastq/*R1*.gz | \
 	sed -r "s:_S[0-9]+_L001_R1_001.fastq.gz::" | \
         sed -r "s:/home/bcrosby/projects/def-pawilson/caribou_MiSeq_project/.*/fastq/::" \
 	> $1_snp_trim/sample_list.txt
 
 
+# Iterate over each sample name found in sample_list.txt, performing trimming and generating quality reports one-by-one for each sample
 while IFS= read -r SAMPLE; do
 
-	echo "# Trimming sample ${SAMPLE} #"
-	echo "# Trimming sample ${SAMPLE} #" >> snp_trim_$SLURM_JOB_ID.err
 
-	java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE \
-		-threads 4 \
-		/home/bcrosby/projects/def-pawilson/caribou_MiSeq_project/$1/fastq/${SAMPLE}_S[0-9]*_L001_R1_001.fastq.gz \
-		/home/bcrosby/projects/def-pawilson/caribou_MiSeq_project/$1/fastq/${SAMPLE}_S[0-9]*_L001_R2_001.fastq.gz \
-		./$1_snp_trim/${SAMPLE}_R1_snp_trim.fastq.gz \
-		./$1_snp_trim/unpaired/${SAMPLE}_R1_unpaired.fastq.gz \
-		./$1_snp_trim/${SAMPLE}_R2_snp_trim.fastq.gz \
-		./$1_snp_trim/unpaired/${SAMPLE}_R2_unpaired.fastq.gz \
-		ILLUMINACLIP:adapter_primer.fa:2:30:10 \
-		SLIDINGWINDOW:4:10 MINLEN:110
+	echo "# Trimming sample ${SAMPLE} #"
+
+
+	# Trimmomatic to quality-trim sequence reads
+	java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE \	# Operate on paired-end sequence data
+		-threads 4 \	# Use 4 cores to speed up process
+		/home/bcrosby/projects/def-pawilson/caribou_MiSeq_project/$1/fastq/${SAMPLE}_S[0-9]*_L001_R1_001.fastq.gz \	# Call the file containing raw forward reads
+		/home/bcrosby/projects/def-pawilson/caribou_MiSeq_project/$1/fastq/${SAMPLE}_S[0-9]*_L001_R2_001.fastq.gz \	# Call the file containing raw reverse reads
+		./$1_snp_trim/${SAMPLE}_R1_snp_trim.fastq.gz \	# Define the filename containing trimmed forward reads
+		./$1_snp_trim/unpaired/${SAMPLE}_R1_unpaired.fastq.gz \	# Define the filename containing dropped forward reads
+		./$1_snp_trim/${SAMPLE}_R2_snp_trim.fastq.gz \	# Define the filename containing trimmed reverse reads
+		./$1_snp_trim/unpaired/${SAMPLE}_R2_unpaired.fastq.gz \	# Define the filename containing dropped reverse reads
+		ILLUMINACLIP:adapter_primer.fa:2:30:10 \	# Remove sequences found in adapter.fa, set quality thresholds
+		SLIDINGWINDOW:4:10 MINLEN:110	# Use sliding window to measure quality of bases, remove any sequence reads less than 110 bp
 
 
 	echo "# Trimming for ${SAMPLE} complete #"
-	echo "# Trimming for ${SAMPLE} complete #" >> snp_trim_$SLURM_JOB_ID.err
+
 
 done < $1_snp_trim/sample_list.txt
 
 
+# Move the job log file ito the snp_trim directory for safe keeping
 mv snp_trim_$SLURM_JOB_ID.log $1_snp_trim/
-mv snp_trim_$SLURM_JOB_ID.err $1_snp_trim/
 
